9506b839cbea27f743fc0d0d679f8aa53f929433
Veerangana-Dash <veeradash@gmail.com>
2018-03-06 22:50:09 +0530

    HTML Format Email

---
+++ htmlformatemail.py
@@ -0,0 +1,52 @@
+#HTML Format Emails
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+
+host="smtp.gmail.com"
+port=587
+username="usertestpy987@gmail.com"
+password="usertestpy"
+from_email= username
+to_list=["usertestpy987@gmail.com"]
+
+try:
+	email_conn= smtplib.SMTP(host,port) 
+	email_conn.ehlo()
+	email_conn.starttls()
+	email_conn.login(username,password)
+
+
+	#server parses this data
+	the_msg = MIMEMultipart("alternative")
+	the_msg['Subject']="Hello"
+	the_msg["From"]= from_email
+	#the_msg["To"]= to_list[0]
+
+	plain_txt = "Testing the message"
+	html_txt= """ \
+	<html>
+		<head></head>
+		<body>
+			<p>Hey!<br>
+				Testing this email <b>message </b>. Compiled by Veerangana Dash.
+			</p>
+		</body>
+	</html>
+	"""
+
+	part_1=MIMEText(plain_txt, 'plain')
+	part_2=MIMEText(html_txt,"html")
+
+	the_msg.attach(part_1)
+	the_msg.attach(part_2)
+
+	#print(the_msg.as_string())
+
+	email_conn.sendmail(from_email,to_list,the_msg.as_string())
+	email_conn.quit()
+except smtplib.SMTPException:
+	print("Error in sending the message")
+
+
+	
